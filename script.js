1b58ea9aff068fa763282f5ab88606ac15ee8aae
jjeremyjacob <THEJEREMYJACOB@GMAIL.COM>
2026-07-30 07:31:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -242,6 +242,18 @@ const nextTrack =
 document.getElementById("nextTrack");
 
 
+for(let i = playlist.length - 1; i > 0; i--){
+
+    const j =
+    Math.floor(
+        Math.random() * (i + 1)
+    );
+
+    [playlist[i], playlist[j]] =
+    [playlist[j], playlist[i]];
+
+}
+
 let currentTrack = 0;
 
 
